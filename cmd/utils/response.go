2c33d31d5e4b33aa332e8307d735c036ef6b5f46
walonCode <mohamedlaminwalonjalloh@gmail.com>
2026-01-06 15:43:20 +0000

    chores: add the required error and success schema

--- cmd/utils/response.go
+++ cmd/utils/response.go
@@ -2,6 +2,19 @@ package utils
 
 import "github.com/gin-gonic/gin"
 
+type SuccessResponseSchema struct {
+	Success bool        `json:"success" example:"true"`
+	Message string      `json:"message" example:"User created"`
+	Data    any `json:"data"`
+}
+
+// ErrorResponseSchema represents an error API response
+type ErrorResponseSchema struct {
+	Success bool   `json:"success" example:"false"`
+	Error   string `json:"error" example:"Invalid request"`
+}
+
+
 func ErrorResponse(c *gin.Context, statusCode int, errorMessage string){
 	c.JSON(
 		statusCode,
@@ -13,12 +26,13 @@ func ErrorResponse(c *gin.Context, statusCode int, errorMessage string){
 }
 
 
-func SuccessResponse(c *gin.Context, statusCode int, message string){
+func SuccessResponse(c *gin.Context, statusCode int, message string, data ...any){
 	c.JSON(
 		statusCode,
 		gin.H{
 			"sucess":true,
 			"message":message,
+			"data": data,
 		},
 	)
 }
